cf88ae383c1d33fd0426d8463d9e12e9c95d91ec
ybonnel <ybonnel@gmail.com>
2012-12-07 10:16:40 +0100

    Mise a jour du script de recuperation des xml avec le changement de serveur

--- getXml.sh
+++ getXml.sh
@@ -5,8 +5,8 @@ rm -rf ${WORK_DIR} 2>/dev/null
 
 mkdir -p ${WORK_DIR}
 
-curl http://transports-rennes.ic-s.org/version/ybo-tv/xml/ | grep "compressed" | cut -d'"' -f8 | while read a
+curl http://ybo-tv.ybonnel.fr/local-xml/ | grep "compressed" | cut -d'"' -f8 | while read a
 do
-	curl http://transports-rennes.ic-s.org/version/ybo-tv/xml/$a > ${WORK_DIR}/$a
+	curl http://ybo-tv.ybonnel.fr/local-xml/$a > ${WORK_DIR}/$a
 done
 
