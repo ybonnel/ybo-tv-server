c929bf6a53323245947bde1bb4adf2dc2f1799a1
ybonnel <ybonnel@gmail.com>
2013-01-12 17:44:35 +0100

    Intégration des nouvelles chaines côté serveur

--- src/main/java/fr/ybo/modele/ChannelForMemCache.java
+++ src/main/java/fr/ybo/modele/ChannelForMemCache.java
@@ -1,11 +1,18 @@
 package fr.ybo.modele;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.primitives.Ints;
 import fr.ybo.xmltv.Channel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.Serializable;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import static com.google.common.collect.Lists.newArrayList;
 
 public class ChannelForMemCache implements Serializable, Comparable<ChannelForMemCache> {
 
@@ -13,128 +20,112 @@ public class ChannelForMemCache implements Serializable, Comparable<ChannelForMe
     private String displayName;
 
 
+    private static final Logger logger = LoggerFactory.getLogger(ChannelForMemCache.class);
+
     @JsonProperty("icon")
     public String getOneIcon() {
-        return mapChaineLogo.get(id);
+        if (!mapChaineLogo.containsKey(id)) {
+            logger.warn("La chaine {} n'a pas de logo", id);
+        }
+        return mapChaineLogo.get(id) + ".png";
     }
 
     private final static Map<String, String> mapChaineLogo = new HashMap<String, String>() {{
-        put("1", "tf1.png");
-        put("2", "france2.png");
-        put("3", "france3.png");
-        put("4", "canal.png");
-        put("5", "france5.png");
-        put("6", "m6.png");
-        put("7", "arte.png");
-        put("8", "d8.png");
-        put("9", "w9.png");
-        put("10", "tmc.png");
-        put("11", "nt1.png");
-        put("12", "nrj12.png");
-        put("13", "france4.png");
-        put("14", "lachaineparlementaire.png");
-        put("15", "bfmtv.png");
-        put("16", "itele.png");
-        put("17", "d17.png");
-        put("18", "gulli.png");
-        put("20", "13erue.png");
-        put("21", "2be.png");
-        put("23", "ab1.png");
-        put("24", "ab3.png");
-        put("25", "ab4.png");
-        put("26", "action.png");
-
-        put("27", "abmoteurs.png");
-        put("28", "alsatictv.png");
-        put("29", "animaux.png");
-        put("38", "boomerang.png");
-        put("42", "canalj.png");
-        put("43", "canalcinema.png");
-        put("44", "canaldecale.png");
-        put("45", "canalfamily.png");
-        put("46", "canalhightech.png");
-        put("47", "canalsport.png");
-        put("48", "canalhorizon.png");
-        put("50", "cartoonnetwork.png");
-        put("51", "chasseetpeche.png");
-        put("52", "cinefirst.png");
-        put("53", "cinefx.png");
-        put("54", "cinepolar.png");
-        put("56", "cinecinemaclassic.png");
-        put("58", "cinecinemaclub.png");
-        put("59", "cinecinemaemotion.png");
-        put("60", "cinecinemafamiz.png");
-        put("61", "cinecinemafrisson.png");
-        put("62", "cinecinemapremier.png");
-        put("65", "cinecinemastar.png");
-        put("67", "clubrtl.png");
-        put("68", "comedie.png");
-        put("69", "cuisinetv.png");
-        put("70", "demaintv.png");
-        put("71", "discoverychannel.png");
-        put("73", "disneychannel.png");
-        put("75", "disneycinemagic.png");
-        put("76", "disneycinemagichd.png");
-        put("80", "eentertainment.png");
-        put("82", "encyclopedia.png");
-        put("83", "equidia.png");
-        put("84", "escales.png");
-        put("85", "espn.png");
-        put("86", "espnclassic.png");
-        put("87", "euronews.png");
-        put("89", "eurosport.png");
-        put("119", "franceo.png");
-        put("121", "gameone.png");
-        put("122", "histoire.png");
-        put("126", "jimmy.png");
-        put("133", "lci.png");
-        put("135", "libertytv.png");
-        put("137", "m6boutique.png");
-        put("138", "m6musicblack.png");
-        put("139", "m6musichits.png");
-        put("142", "mangas.png");
-        put("144", "mcm.png");
-        put("146", "mcmpop.png");
-        put("147", "mcmtop.png");
-        put("148", "mezzo.png");
-        put("149", "motorstv.png");
-        put("150", "mtv.png");
-        put("162", "mtvbase.png");
-        put("164", "mtvidol.png");
-        put("165", "mtvpulse.png");
-        put("168", "nationalgeographic.png");
-        put("169", "nationalgeographichd.png");
-        put("171", "nickelodeon.png");
-        put("173", "nrjhits.png");
-        put("174", "nrjparis.png");
-        put("175", "odyssee.png");
-        put("176", "oltv.png");
-        put("177", "omtv.png");
-        put("186", "parispremiere.png");
-        put("187", "pinktv.png");
-        put("188", "planete.png");
-        put("190", "planetejustice.png");
-        put("191", "planetenolimit.png");
-        put("192", "planetethalassa.png");
-        put("199", "rtl9.png");
-        put("201", "syfy.png");
-        put("202", "seasons.png");
-        put("203", "serieclub.png");
-        put("206", "tcm.png");
-        put("214", "telenantes.png");
-        put("220", "teletoon.png");
-        put("227", "teva.png");
-        put("228", "tf6.png");
-        put("229", "tiji.png");
-        put("233", "tpsstar.png");
-        put("237", "tv5monde.png");
-        put("243", "tvbreizh.png");
-        put("246", "ushuaiatv.png");
-        put("247", "vivolta.png");
-        put("248", "voyage.png");
-        put("253", "cnn.png");
-        put("288", "france24.png");
-        put("294", "idf1.png");
+        put("13R1", "13erue");
+        put("6TER", "6ter");
+        put("MOT1", "abmoteurs");
+        put("AB11", "ab1");
+        put("AB31", "ab3");
+        put("AB41", "ab4");
+        put("ACT1", "action");
+        put("ANI1", "animaux");
+        put("ART1", "arte");
+        put("BEI1", "beinsport1");
+        put("BEI2", "beinsport2");
+        put("BFM1", "bfmtv");
+        put("BOO1", "boomerang");
+        put("CIN20", "cinecinemaclassic");
+        put("CIN21", "cinecinemaclub");
+        put("CIN18", "cinecinemafamiz");
+        put("CIN22", "cinecinemafrisson");
+        put("CIN23", "cinecinemapremier");
+        put("CIN28", "cinecinemastar");
+        put("CAN1", "canalj");
+        put("CAN2", "canal");
+        put("CAN3", "canalcinema");
+        put("CAN4", "canaldecale");
+        put("CAN10", "canalfamily");
+        put("CAN5", "canalsport");
+        put("CAR1", "cartoonnetwork");
+        put("CHE1", "cherie25");
+        put("CIN1", "cinefx");
+        put("CIN2", "cinepolar");
+        put("CIN19", "cinefirst");
+        put("CO11", "comedie");
+        put("DIR1", "d8");
+        put("EUR2", "d17");
+        put("DIS1", "discoverychannel");
+        put("DIS3", "disneychannel");
+        put("DIS5", "disneycinemagic");
+        put("EEN1", "eentertainment");
+        put("EQU1", "equidia");
+        put("ESC1", "escales");
+        put("ESP1", "espnclassic");
+        put("FRA2", "france2");
+        put("FRA3", "france3");
+        put("FRA4", "france4");
+        put("FRA5", "france5");
+        put("FRA1", "franceo");
+        put("GAM1", "gameone");
+        put("GUL1", "gulli");
+        put("HD1", "hd1");
+        put("ITL1", "itele");
+        put("JIM1", "jimmy");
+        put("LEQ1", "lequipe21");
+        put("LAD1", "ladeux");
+        put("LAU1", "laune");
+        put("LAC1", "lachaineparlementaire");
+        put("M61", "m6");
+        put("M6M2", "m6musichits");
+        put("MAC1", "machainesport");
+        put("MAN1", "mangas");
+        put("MCM1", "mcm");
+        put("MEZ1", "mezzo");
+        put("MOT2", "motorstv");
+        put("MTV1", "mtv");
+        put("NAT1", "nationalgeographic");
+        put("NIC1", "nickelodeon");
+        put("NOL1", "nolife");
+        put("NRJ1", "nrj12");
+        put("NT11", "nt1");
+        put("NU23", "numero23");
+        put("OMT1", "omtv");
+        put("PAR1", "parispremiere");
+        put("PIN1", "pinktv");
+        put("PLA1", "planete");
+        put("PLA5", "planetejustice");
+        put("PLA2", "planetenolimit");
+        put("PLA3", "planetethalassa");
+        put("DIS15", "playhousedisney");
+        put("RMC2", "rmcdecouverte");
+        put("RTL2", "rtl9");
+        put("SEA1", "seasons");
+        put("SPO1", "sportplus");
+        put("TCM1", "tcm");
+        put("TLT1", "teletoon");
+        put("TVA1", "teva");
+        put("TF11", "tf1");
+        put("TF61", "tf6");
+        put("TIJ1", "tiji");
+        put("TMC1", "tmc");
+        put("TOU1", "toutelhistoire");
+        put("TSR1", "tsr1");
+        put("TSR2", "tsr2");
+        put("TVB1", "tvbreizh");
+        put("TV51", "tv5monde");
+        put("USH1", "ushuaiatv");
+        put("VIV1", "vivolta");
+        put("W91", "w9");
     }};
 
     public String getId() {
@@ -171,10 +162,51 @@ public class ChannelForMemCache implements Serializable, Comparable<ChannelForMe
     }
 
 
+    private final static Map<String, Integer> mapChaineNumero = new HashMap<String, Integer>() {{
+        put("TF11", 1);
+        put("FRA2", 2);
+        put("FRA3", 3);
+        put("CAN2", 4);
+        put("FRA5", 5);
+        put("M61", 6);
+        put("ART1", 7);
+        put("DIR1", 8);
+        put("W91", 9);
+        put("TMC1", 10);
+        put("NT11", 11);
+        put("NRJ1", 12);
+        put("LAC1", 13);
+        put("FRA4", 14);
+        put("BFM1", 15);
+        put("ITL1", 16);
+        put("EUR2", 17);
+        put("GUL1", 18);
+        put("FRA1", 19);
+        put("HD1", 20);
+        put("LEQ1", 21);
+        put("6TER", 22);
+        put("NU23", 23);
+        put("RMC2", 24);
+        put("CHE1", 25);
+        put("PAR1", 50);
+        put("TVA1", 51);
+        put("RTL2", 52);
+    }};
+
+    private static int getNumero(String idChaine) {
+        if (mapChaineNumero.containsKey(idChaine)) {
+            return mapChaineNumero.get(idChaine);
+        } else {
+            return 999;
+        }
+    }
+
     @Override
     public int compareTo(ChannelForMemCache o) {
-        int id1 = Integer.parseInt(id);
-        int id2 = Integer.parseInt(o.id);
-        return (id1 == id2) ? 0 : (id1 < id2) ? -1 : 1;
+        int result = Ints.compare(getNumero(id), getNumero(o.id));
+        if (result == 0) {
+            result = id.compareTo(o.id);
+        }
+        return result;
     }
 }
--- src/main/java/fr/ybo/util/GetTv.java
+++ src/main/java/fr/ybo/util/GetTv.java
@@ -47,93 +47,6 @@ public class GetTv {
         return programmes;
     }
 
-    private final static Set<String> channelFilterred = new HashSet<String>() {{
-        add("40");
-        add("49");
-        add("55");
-        add("63");
-        add("64");
-        add("90");
-        add("91");
-        add("123");
-        add("124");
-        add("132");
-        add("160");
-        add("161");
-        add("163");
-        add("170");
-        add("185");
-        add("211");
-        add("234");
-        add("240");
-        add("249");
-        add("250");
-        add("279");
-        add("282");
-        add("289");
-        add("297");
-        add("302");
-        add("304");
-        add("901");
-
-        // Ajouter au filter pour cause mémoire.
-        add("31");
-        add("32");
-        add("33");
-        add("34");
-        add("35");
-        add("78"); //"dorceltv.png");
-        add("79"); //"ducotedechezvoustv.png");
-        add("92"); //"june.png");
-        add("125"); //"jetix.png");
-        add("130"); //"ladeux.png");
-        add("131"); //"laune.png");
-        add("141"); //"machainesport.png");
-        add("180"); //"orangecinegeants.png");
-        add("181"); //"orangecinechoc.png");
-        add("182"); //"orangecinehappy.png");
-        add("183"); //"orangecinemax.png");
-        add("194"); //"playhousedisney.png");
-        add("195"); //"plugrtl.png");
-        add("204"); //"sportplus.png");
-        add("209"); //"teleguadeloupe.png");
-        add("210"); //"teleguyane.png");
-        add("213"); //"telemartinique.png");
-        add("216"); //"telenouvellecaledonie.png");
-        add("217"); //"telepolynesie.png");
-        add("218"); //"telereunion.png");
-        add("222"); //"telif.png");
-        add("235"); //"tsr1.png");
-        add("236"); //"tsr2.png");
-        add("275"); //"2mmonde.png");
-        add("276"); //"piwi.png");
-        add("278"); //"3sat.png");
-        add("279"); //"bloombergtv.png");
-        add("281"); //"yachtsail.png");
-        add("290"); //"babyfirst.png");
-        add("293"); //"cap24.png");
-        add("295"); //"etb1.png");
-        add("296"); //"etb2.png");
-        add("298"); //"m6musicclub.png");
-        add("299"); //"toutelhistoire.png");
-        add("301"); //"ard.png");
-        add("303"); //"zdf.png");
-        add("304"); //"dsf.png");
-        add("401"); //"bbcone.png");
-        add("402"); //"bbctwo.png");
-        add("403"); //"bbcprime.png");
-        add("511"); //"rtbfsat.png");
-        add("514"); //"rtltvi.png");
-        add("601"); //"tve1.png");
-        add("602"); //"tve2.png");
-        add("603"); //"tveinternational.png");
-        add("701"); //"raiuno.png");
-        add("702"); //"raidue.png");
-        add("703"); //"raitre.png");
-        add("811"); //"latrois.png");
-
-    }};
-
     private static List<ChannelForMemCache> getTvFromNetworkAndReturnChannels(String currentDate) throws JAXBException, IOException {
         TvForMemCache tv;
         synchronized (jeton) {
@@ -142,17 +55,10 @@ public class GetTv {
 
             tv = TvForMemCache.fromTv((Tv) um.unmarshal(GetZip.getFileForChannels()));
 
-            Iterator<ChannelForMemCache> itChannel = tv.getChannel().iterator();
-            while (itChannel.hasNext()) {
-                if (channelFilterred.contains(itChannel.next().getId())) {
-                    itChannel.remove();
-                }
-            }
-
             CacheService.getInstance().addChannels(currentDate, tv.getChannel());
 
             for (ChannelForMemCache channel : tv.getChannel()) {
-                logger.info("Récupération des programmes de la chaine " + channel.getDisplayName());
+                logger.info("Récupération des programmes de la chaine " + channel.getDisplayName() + "(id=" + channel.getId() + ")");
                 TvForMemCache tvWithProgrammes = TvForMemCache.fromTv((Tv) um.unmarshal(GetZip.getFileProgrammeForOneChannel(channel.getId())));
                 CacheService.getInstance().addProgrammes(currentDate, channel.getId(), tvWithProgrammes.getProgramme());
             }
@@ -170,13 +76,6 @@ public class GetTv {
             List<ChannelForMemCache> channels = CacheService.getInstance().getChannels(currentDate);
             if (channels == null) {
                 TvForMemCache tv = TvForMemCache.fromTv((Tv) um.unmarshal(GetZip.getFileForChannels()));
-
-                Iterator<ChannelForMemCache> itChannel = tv.getChannel().iterator();
-                while (itChannel.hasNext()) {
-                    if (channelFilterred.contains(itChannel.next().getId())) {
-                        itChannel.remove();
-                    }
-                }
                 channels = tv.getChannel();
                 CacheService.getInstance().addChannels(currentDate, channels);
             }
@@ -206,13 +105,6 @@ public class GetTv {
             Unmarshaller um = jc.createUnmarshaller();
 
             TvForMemCache tv = TvForMemCache.fromTv((Tv) um.unmarshal(GetZip.getFileForChannels()));
-
-            Iterator<ChannelForMemCache> itChannel = tv.getChannel().iterator();
-            while (itChannel.hasNext()) {
-                if (channelFilterred.contains(itChannel.next().getId())) {
-                    itChannel.remove();
-                }
-            }
 
             CacheService.getInstance().addChannels(currentDate, tv.getChannel());
 
